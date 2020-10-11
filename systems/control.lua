e16295161cf5f3c869ef4e453e3cf0d5ec2678a3
gabrielbosio <gabrielbosio95@gmail.com>
2020-10-10 21:05:35 -0300

    Fixed flyingHurt transition bug

--- systems/control.lua
+++ systems/control.lua
@@ -128,8 +128,8 @@ local statesLogic = {
       end
     end
 
-    if not (args.living.health and args.collisionBox.hurtFallHeight
-        and mustFly) then 
+    if not args.living.health and not (args.collisionBox.hurtFallHeight
+        and mustFly) then
       args.stateMachine:setState("hit", 0.5)
       args.animationClip:setAnimation("hitByHighPunch")
     end
