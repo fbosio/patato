d90c6dbac23b33dc55f526e7c7fe3906a0d1ddc9
gabrielbosio <gabrielbosio95@gmail.com>
2020-10-17 00:22:58 -0300

    Sprites and animations of Patato climbing and bee

--- resources/animations.lua
+++ resources/animations.lua
@@ -6,15 +6,22 @@ return {
   patato = {
     standing = {{{1, 1}}, false},
     walking = {{{2, 0.1}, {3, 0.1}, {4, 0.1}, {3, 0.1}}, true},
-	startingJump = {{{1, 0.04}}, false},
-	jumping = {{{4, 1}}, false},
-	crouching = {{{5, 1}}, false},
-	crouchWalking = {{{6, 0.1}, {7, 0.1}, {8, 0.1}, {7, 0.1}}, true},
-	standingAttackingFlySwat = {{{9, 0.1}, {10, 0.1}, {9, 0.1}}, false},
-	crouchingingAttackingFlySwat = {{{11, 0.1}, {12, 0.1}, {11, 0.1}}, false},
-	flyingHurt = {{{14, 1}}, false},
-	lyingDown = {{{15, 1}}, false},
-	gettingUp = {{{5, 0.1}}, false},
-	hit = {{{13, 1}}, false},
+    startingJump = {{{1, 0.04}}, false},
+    jumping = {{{4, 1}}, false},
+    crouching = {{{5, 1}}, false},
+    crouchWalking = {{{6, 0.1}, {7, 0.1}, {8, 0.1}, {7, 0.1}}, true},
+    standingAttackingFlySwat = {{{9, 0.1}, {10, 0.1}, {9, 0.1}}, false},
+    crouchingingAttackingFlySwat = {{{11, 0.1}, {12, 0.1}, {11, 0.1}}, false},
+    climbingIdle = {{{17, 1}}, false},
+    climbingUp = {{{18, 0.1}, {17, 0.1}, {16, 0.1}, {17, 0.1}}, true},
+    climbingDown = {{{16, 0.1}, {17, 0.1}, {18, 0.1}, {17, 0.1}}, true},
+    climbingAttackingFlySwat = {{{19, 0.1}, {20, 0.1}, {19, 0.1}}, false},
+    flyingHurt = {{{14, 1}}, false},
+    lyingDown = {{{15, 1}}, false},
+    gettingUp = {{{5, 0.1}}, false},
+    hit = {{{13, 1}}, false},
+  },
+  bee = {
+    flying = {{{21, 0.05}, {22, 0.05}}, true}
   }
 }
