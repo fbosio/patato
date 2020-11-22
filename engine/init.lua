17528e7c8fa92daefd5c7147841e199d33695808
gabrielbosio <gabrielbosio95@gmail.com>
2020-11-22 01:11:20 -0300

    Added default values to world

--- engine/init.lua
+++ engine/init.lua
@@ -3,15 +3,9 @@ local tinyyaml = require "tinyyaml"
 local M = {}
 
 function M.load (config_yaml)
-  local config
-  if #config_yaml > 0 then
-    config = tinyyaml.parse(config_yaml)
-  else
-    config = {
-      world = {gravity = 0}
-    }
-  end
-  M.world = config.world
+  local config = #config_yaml > 0 and tinyyaml.parse(config_yaml) or {}
+  M.world = config.world or {}
+  M.world.gravity = M.world.gravity or 0
 end
 
 return M
--- engine/spec/loadconfig_spec.lua
+++ engine/spec/loadconfig_spec.lua
@@ -10,3 +10,14 @@ describe("Load with empty config", function ()
     assert.are.same(0, engine.world.gravity)
   end)
 end)
+
+describe("Load with empty world", function ()
+  it("should load a world with zero gravity", function ()
+    config = "world:"
+    config_mock = mock(config)
+
+    engine.load(config_mock)
+
+    assert.are.same(0, engine.world.gravity)
+  end)
+end)
