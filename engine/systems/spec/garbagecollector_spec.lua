c1fa6f1bc0e190319133834a85644e45f1b2349e
gabrielbosio <gabrielbosio95@gmail.com>
2020-12-20 18:58:40 -0300

    Test: do not remove components from other entities
    Co-authored-by: fbosio <fedebosio93@gmail.com>

--- engine/systems/spec/garbagecollector_spec.lua
+++ engine/systems/spec/garbagecollector_spec.lua
@@ -9,8 +9,10 @@ after_each(function ()
 end)
 
 describe("Loading an entity that is marked as garbage", function ()
-  it("should remove all of its components", function ()
-    local gameState = {
+  local gameState
+
+  before_each(function ()
+    gameState = {
       garbage = {
         markedEntity = true
       },
@@ -18,6 +20,10 @@ describe("Loading an entity that is marked as garbage", function ()
         markedEntity = {
           x = 45,
           y = 289
+        },
+        nonMarkedEntity = {
+          x = 40,
+          y = 270
         }
       },
       collectable = {
@@ -26,22 +32,39 @@ describe("Loading an entity that is marked as garbage", function ()
       collisionBox = {
         markedEntity = {
           origin = {
-            x = 35,
-            y = 100
+            x = 25,
+            y = 25
+          },
+          width = 50,
+          height = 50,
+          x = 45,
+          y = 289
+        },
+        nonMarkedEntity = {
+          origin = {
+            x = 20,
+            y = 135
           },
           width = 70,
           height = 100,
-          x = 45,
-          y = 289
+          x = 40,
+          y = 270
         }
       }
     }
-
+    
     garbagecollector.update(gameState)
+  end)
 
+  it("should remove all of its components", function ()
     assert.is.falsy(gameState.garbage.markedEntity)
     assert.is.falsy(gameState.position.markedEntity)
     assert.is.falsy(gameState.collectable.markedEntity)
     assert.is.falsy(gameState.collisionBox.markedEntity)
+  end)
+
+  it("should not remove components from other entities", function ()
+    assert.is.truthy(gameState.position.nonMarkedEntity)
+    assert.is.truthy(gameState.collisionBox.nonMarkedEntity)
   end)
 end)
