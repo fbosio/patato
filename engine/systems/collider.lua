d3d4712db757e9826407b03536dad64fd50e557d
fbosio <fedebosio93@gmail.com>
2020-12-27 22:38:32 -0300

    Finished collider tests: four directions

--- engine/systems/collider.lua
+++ engine/systems/collider.lua
@@ -7,7 +7,7 @@ function M.update(dt, solids, collideables, collisionBoxes, positions,
     local position1 = positions[solidEntity]
     local velocity1 = velocities[solidEntity]
     local x1, y1 = position1.x - box1.origin.x, position1.y - box1.origin.y
-    
+
     for collideableEntity, _ in pairs(collideables or {}) do
       local box2 = collisionBoxes[collideableEntity]
       local position2 = positions[collideableEntity]
@@ -21,11 +21,26 @@ function M.update(dt, solids, collideables, collisionBoxes, positions,
       local right2 = x2 + box2.width
       local top2 = y2
       local bottom2 = y2 + box2.height
-  
+
       if right1 <= left2 and right1 + velocity1.x*dt > left2
           and top1 < bottom2 and bottom1 > top2 then
         velocity1.x = 0
         position1.x = left2 - box1.width + box1.origin.x
+      end
+      if left1 >= right2 and left1 + velocity1.x*dt < right2
+          and top1 < bottom2 and bottom1 > top2 then
+        velocity1.x = 0
+        position1.x = right2 + box1.origin.x
+      end
+      if top1 >= bottom2 and top1 + velocity1.y*dt < bottom2
+          and left1 < right2 and right1 > left2 then
+        velocity1.y = 0
+        position1.y = bottom2 + box1.origin.y
+      end
+      if bottom1 <= top2 and bottom1 + velocity1.y*dt > top2
+          and left1 < right2 and right1 > left2 then
+        velocity1.y = 0
+        position1.y = top2 - box1.height + box1.origin.y
       end
     end
   end
--- engine/systems/spec/collider_spec.lua
+++ engine/systems/spec/collider_spec.lua
@@ -1,5 +1,23 @@
 local dt = 1 / 70
 local collider
+local solids = {
+  mario = true
+}
+local collideables = {
+  block = {name = "block"}
+}
+local collisionBoxes = {
+  mario = {
+    origin = {x = 16, y = 64},
+    width = 32,
+    height = 64
+  },
+  block = {
+    origin = {x = 16, y = 32},
+    width = 32,
+    height = 64
+  }
+}
 
 before_each(function ()
   collider = require "engine.systems.collider"
@@ -9,33 +27,15 @@ after_each(function ()
   package.loaded["engine.systems.collider"] = nil
 end)
 
-describe("loading a player that collides with a block", function ()
-  it("should stop the player so it does not go through the block", function ()
-    local solids = {
-      mario = true
-    }
-    local collideables = {
-      block = {name = "block"}
-    }
-    local collisionBoxes = {
-      mario = {
-        origin = {x = 16, y = 64},
-        width = 32,
-        height = 64
-      },
-      block = {
-        origin = {x = 16, y = 32},
-        width = 32,
-        height = 64
-      }
-    }
+describe("with a player touching the left side of a block", function ()
+  it("should stop the player and push it to the left", function ()
     local positions = {
       mario = {
         x = 280,
         y = 380
       },
       block = {
-        x = 316,
+        x = 320,
         y = 380
       }
     }
@@ -49,7 +49,88 @@ describe("loading a player that collides with a block", function ()
     collider.update(dt, solids, collideables, collisionBoxes, positions,
                     velocities)
 
-    assert.are.same(284, positions.mario.x)
+    assert.are.same(288, positions.mario.x)
     assert.are.same(0, velocities.mario.x)
+  end)
+end)
+
+describe("with a player touching the right side of a block", function ()
+  it("should stop the player and push it to the right", function ()
+    local positions = {
+      mario = {
+        x = 366,
+        y = 380
+      },
+      block = {
+        x = 320,
+        y = 380
+      }
+    }
+    local velocities = {
+      mario = {
+        x = -1400,
+        y = 0
+      }
+    }
+
+    collider.update(dt, solids, collideables, collisionBoxes, positions,
+                    velocities)
+
+    assert.are.same(352, positions.mario.x)
+    assert.are.same(0, velocities.mario.x)
+  end)
+end)
+
+describe("with a player touching the bottom of a block", function ()
+  it("should stop the player and push it to the bottom", function ()
+    local positions = {
+      mario = {
+        x = 346,
+        y = 486
+      },
+      block = {
+        x = 320,
+        y = 380
+      }
+    }
+    local velocities = {
+      mario = {
+        x = 0,
+        y = -1400
+      }
+    }
+
+    collider.update(dt, solids, collideables, collisionBoxes, positions,
+                    velocities)
+
+    assert.are.same(476, positions.mario.y)
+    assert.are.same(0, velocities.mario.y)
+  end)
+end)
+
+describe("with a player touching the top of a block", function ()
+  it("should stop the player and push it to the top", function ()
+    local positions = {
+      mario = {
+        x = 346,
+        y = 338
+      },
+      block = {
+        x = 320,
+        y = 380
+      }
+    }
+    local velocities = {
+      mario = {
+        x = 0,
+        y = 1400
+      }
+    }
+
+    collider.update(dt, solids, collideables, collisionBoxes, positions,
+                    velocities)
+
+    assert.are.same(348, positions.mario.y)
+    assert.are.same(0, velocities.mario.y)
   end)
 end)
