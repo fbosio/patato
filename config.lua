a0aeba31fd643e338fc0f839e9d462e1be844135
gabrielbosio <gabrielbosio95@gmail.com>
2020-12-29 21:51:45 -0300

    Removed from README old default inputs

--- config.lua
+++ config.lua
@@ -1,105 +1,33 @@
 local M = {}
 
-M.physics = {
-  gravity = 5000
-}
-
-M.keys = {
-  left2 = "j",
-  right2 = "l",
-  up2 = "i",
-  down2 = "k",
-  start = "return",
-  message = "m"
-}
-
-M.spriteSheet = "resources/sprites/patato.png"
-M.spriteScale = 0.5
-M.sprites = {
-  {1, 1, 137, 266, 72.35, 256.5}, -- {x, y, width, height, originX, originY}
-  {138, 1, 205, 251, 96.35, 251.5},
-  {343, 1, 134, 282, 55.349999999999994, 271.5},
-  {477, 1, 190, 264, 101.35, 259.5},
-  {917, 833, 61, 83, 28.349999999999994, 80.5}
-}
-
 M.entities = {
-  patato = {
+  player = {
     input = {
       walkLeft = "left",
       walkRight = "right",
       walkUp = "up",
-      walkDown = "down",
-      showCustomMessage = "message"
+      walkDown = "down"
     },
     collector = true,
-    collisionBox = {20, 120, 40, 120},
---    animations = {
---      walking = {2, 0.1, 3, 0.1, 4, 0.1, 3, 0.1, true},
---      standing = {1, 1, false} -- {spr1, t1, spr2, t2, ..., looping}
---    },
-    solid = true,
-    -- gravitational = true
+    collisionBox = {15, 35, 30, 70}
   },
-   bee = {
-     input = {
-       walkLeft = "left2",
-       walkRight = "right2",
-     }
-   },
-  bottle = {
+  coin = {
     collectable = true,
-    collisionBox = {15, 45, 35, 45},
-    animations = {
-      idle = {5, 1, false}
-    }
-  },
-  surface = {
-    collideable = "rectangle"
-  },
-  slope = {
-    collideable = "triangle"
-  },
-  mainMenu = {
-    input = {
-      menuPrevious = "up",
-      menuNext = "down",
-      menuSelect = "start"
-    },
-    menu = {
-      options = {"Start", "Show message", "Secret level"},
-    },
+    collisionBox = {5, 5, 10, 10}
   }
 }
 
 M.levels = {
-  garden = {
-    patato = {110, 239},
-    bee = {450, 105},
-    bottle = {
-      {325, 80},
-      {385, 80},
-      {445, 80},
-      {505, 80},
-      {565, 80},
-      {600, 500},
-    },
-    surface = {
-      {170, 280, 255, 355},  -- block: x1, y1, x2, y2
-      {255, 280, 340, 355},
-      {170, 355, 255, 430},
-      {255, 355, 340, 430},
-      {300, 80, 600}  -- cloud: x1, y1, x2
-    },
-    slope = {
-      {620, 230, 680, 170},
-      {740, 230, 680, 170},
+  myLevel = {
+    player = {260, 300},
+    coin = {
+      {440, 300},
+      {460, 300},
+      {480, 300},
+      {500, 300},
+      {520, 300}
     }
-  },
-  secretLevel = {
-    patato = {200, 500},
-  },
+  }
 }
-M.firstLevel = "garden"
 
 return M
--- main.lua
+++ main.lua
@@ -1,98 +1,23 @@
 local engine = require "engine"
-local elapsed, message, score
+local score
 
 
 function love.load()
   engine.load()
 
-  engine.setMenuOptionEffect("mainMenu", 1, function ()
-    engine.startGame()  -- changeScene (call automatically if there is no menu)
-  end)
-  engine.setMenuOptionEffect("mainMenu", 2, function ()
-    elapsed = 0
-    message = "Hola, mundo"
-  end)
-  engine.setMenuOptionEffect("mainMenu", 3, function ()
-    engine.startGame("secretLevel")
-  end)
-
-  engine.setAction("jump", function (c)
-    if c.velocity.y == 0 then
-      c.velocity.y = -c.impulseSpeed.jump
-    end
-  end)
-  engine.setAction("showCustomMessage", function ()
-    message = "Flashlight!"
-  end)
-  engine.setOmissions({"showCustomMessage"}, function ()
-    message = ""
-  end)
-
-  engine.setAction("walkLeft", function (c)
-    c.velocity.x = -c.impulseSpeed.walk
-    c.animation.name = "walking"
-  end)
-  engine.setAction("walkRight", function (c)
-    c.velocity.x = c.impulseSpeed.walk
-    c.animation.name = "walking"
-  end)
-  engine.setAction("walkUp", function (c)
-    c.velocity.y = -c.impulseSpeed.walk
-    c.animation.name = "walking"
-  end)
-  engine.setAction("walkDown", function (c)
-    c.velocity.y = c.impulseSpeed.walk
-    c.animation.name = "walking"
-  end)
-  engine.setOmissions({"walkLeft", "walkRight", "walkUp", "walkDown"},
-    function (c)
-      c.animation.name = "standing"
-    end
-  )
-  for k, v in pairs(engine.hid.omissions) do
-    local text = "[{"
-    for _, name in ipairs(k) do
-      text = text .. '"' .. name .. '", '
-    end
-    text = text .. "}] = " .. tostring(v)
-    print(text)
-  end
-
   score = 0
-  engine.setCollectableEffect("bottle", function ()
+  -- setCollectableEffect(name, callback)
+  engine.setCollectableEffect("coin", function ()
     score = score + 1
   end)
 end
 
 function love.update(dt)
   engine.update(dt)
-
-  if elapsed then
-    elapsed = elapsed + dt
-    if elapsed > 1 then
-      elapsed = nil
-      message = nil
-    end
-  end
 end
 
 function love.draw()
   engine.draw()
 
-  if message then
-    love.graphics.print(message, 0, 0)
-  end
-
-  if not engine.gameState.inMenu then
-    love.graphics.print(score, 0, 0)
-  end
-
-
-  local mouseX, mouseY = love.mouse.getPosition()
-  love.graphics.print(tostring(mouseX) .. ", " .. tostring(mouseY),
-                      mouseX + 10, mouseY - 10)
-end
-
-function love.keypressed(key)
-  engine.keypressed(key)
+  love.graphics.print(score, 0, 0)
 end
