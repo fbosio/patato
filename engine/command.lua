57a28215a93ecec1927fd92175488fd6f8cceeb1
gabrielbosio <gabrielbosio95@gmail.com>
2021-01-15 21:34:07 -0300

    Made command test pass
    Co-authored-by: fbosio <fedebosio93@gmail.com>

--- engine/command.lua
+++ engine/command.lua
@@ -5,4 +5,39 @@ function M.load(hid, components)
   M.components = components
 end
 
+-- Old code
+-- Check that all values of table t1 are in table t2.
+local function isIncluded(t1, t2)                                                                   
+  for _, v1 in ipairs(t1) do                                                                        
+    local hasValue = false                                                                          
+    for _, v2 in ipairs(t2) do                                                                      
+      if v1 == v2 then                                                                              
+        hasValue = true                                                                             
+        break                                                                                       
+      end                                                                                           
+    end                                                                                             
+    if not hasValue then                                                                            
+      return false                                                                                  
+    end                                                                                             
+  end                                                                                               
+  return true                                                                                       
+end
+
+local mt = {
+  __eq = function (a, b)
+    return not a.release == not b.release and not a.oneShot == not b.oneShot
+      and isIncluded(a.input, b.input) and isIncluded(b.input, a.input)
+  end
+}
+
+function M.new(args)
+  local newCommand = {
+    release = args.release,
+    oneShot = args.oneShot,
+    input = type(args.input) == "table" and args.input or {args.input}
+  }
+  setmetatable(newCommand, mt)
+  return newCommand
+end
+
 return M
